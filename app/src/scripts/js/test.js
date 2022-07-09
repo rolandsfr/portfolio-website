dd20bfbb82b5b7c49108612730d4407a9892588a
rolandsfr <rolands.affaires@gmail.com>
2022-07-09 14:14:39 +0300

    description updated

---
+++ app/src/scripts/js/test.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var locomotive_scroll_1 = require("locomotive-scroll");
+var scroll = new locomotive_scroll_1["default"]({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true
+});
